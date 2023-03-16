37132fda19b7078b8042e158b3776023657bd352
JP Merten <kc8son@yahoo.com>
2023-03-15 21:02:33 -0500

    initial checkpoint

--- Day008/main.py
+++ Day008/main.py
@@ -10,3 +10,37 @@ import sys
 
 ####################################################################################################
 #   Variables
+menu_list = {
+    "Tomatoes":     2.50,
+    "Bread":        1.25,
+    "Mushrooms":    5.79,
+    "Cereal":       1.89,
+    "Butter":       3.15,
+    "Eggs":         2.10,
+    "Lettuce":      1.35
+}
+
+####################################################################################################
+#   functions
+def print_menu():
+    i = 1
+    print("Today's menu:\nPlease make a selection or type 'checkout'")
+    for key in menu_list.keys():
+        print(f"{i} - {key} - ${menu_list[key]:.2f}")
+        i += 1
+
+def get_selection():
+    resonse = input("> ")
+    #   Test 1 - check for numeric entry
+    if resonse.isnumeric() and 0<
+    print(x)
+    #   Test 2 - scan for the full name
+    #   Test 3 - scan for partiao name.
+
+####################################################################################################
+#   lambdas
+
+###################################################################################################
+#   Main routine.
+print_menu()
+get_selection()
