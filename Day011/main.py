fef14c18c3cfb7655f2451d2d45fb359ff3dcd26
JP Merten <kc8son@yahoo.com>
2023-03-23 07:58:03 -0500

    Added a validator class for the extended challenge.

--- Day011/main.py
+++ Day011/main.py
@@ -3,6 +3,9 @@
 #   Date Written: 03/22/2023        By: Joseph P. Merten
 #   Day 10: Circle Challenge - Python OOP: Classes
 #
+#   Date Modified: 03/23/2023       By: Joseph P. Merten
+#   Added a validator class for the extended challenge.
+#
 ####################################################################################################
 #   imports
 import pdb
@@ -25,15 +28,39 @@ class Circle():
     def get_radius(self):
         return self.radius
 
+class Validator():
+    def validate_radius():
+        """This METHOD will get a valid decimal number."""
+        print("Enter a radius:")
+        my_radius = -1.0
+        while my_radius < 0.0:
+            my_response = input("> ")
+            try:
+                my_radius = float(my_response)
+                if my_radius < 0:
+                    print("Please enter a positive value.")
+            except:
+                print("Invalid entry, please enter a floating point number...")
+                my_radius = -1
+        return my_radius
+    def validate_yn():
+        "This METHOD determines if we want to grow the circle larger."
+        my_resp = 'x'
+        while my_resp != 'n' and my_resp != 'y':
+            my_resp = input("Would you like your circle to grow?\n> ")
+            if my_resp != 'n' and my_resp != 'y':
+                print("Invalid response, please enter 'y' or 'n'...")
+        return my_resp
+
 ####################################################################################################
 #   Variables
 grow_flag = 'y'
+validated_radius = -1.0
 
 ####################################################################################################
 #   Functions
 def input_radius():
     """This function will get a valid decimal number."""
-    print("Welcome to the circle Tester")
     print("Enter a radius:")
     my_radius = -1
     while my_radius < 0:
@@ -52,6 +79,7 @@ def output_values(my_circle):
     print(f"Diameter: {my_circle.calculate_diameter()}")
     print(f"Circumference: {my_circle.calculate_circumference()}")
     print(f"Area: {my_circle.calculate_area()}")
+
 def get_grow_flag(my_circle):
     "This determines if we want to grow the circle another unit larger."
     my_resp = 'x'
@@ -73,8 +101,17 @@ def get_grow_flag(my_circle):
 
 ####################################################################################################
 #   Main code
-g_circle = Circle(input_radius())
+print("Welcome to the circle Tester")
+# g_circle = Circle(input_radius())
+g_circle = Circle(Validator.validate_radius())
 while grow_flag.lower() == 'y':
     output_values(g_circle)
-    grow_flag = get_grow_flag(g_circle)
+    # grow_flag = get_grow_flag(g_circle)
+    grow_flag = Validator.validate_yn()
+    if grow_flag == 'y':
+        print("Stand by while your circle magically grows")
+        g_circle.grow()
+    else:
+        print(f"The last radius of your circle was: {g_circle.get_radius()}")
+        print("Goodbye")
 
