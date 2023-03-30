ec7b3d538616614dafa93538c65b1309108d53c8
JP Merten <kc8son@yahoo.com>
2023-03-30 07:54:05 -0500

    Removed a duplicate line and made a note about a different approach for the counts.

--- Day013/checkers.py
+++ Day013/checkers.py
@@ -14,11 +14,11 @@ from numpy import random
 def build_board(board_size):
     """This function will build a 2 dimensional board of the specified size."""
     my_board = random.choice(["Red", "Black", "Empty"], (board_size, board_size))
-    my_board = random.choice(["Red", "Black", "Empty"], (board_size, board_size))
     return my_board
 
 def get_count(my_board, cell_val):
     """This function will count how many positions for a specific color there are."""
+    # cell_counts = (my_board == cell_val).count()  #   research this approach
     cell_counts = 0
     for row in my_board:
         for cell in row:
