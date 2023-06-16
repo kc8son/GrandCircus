10e8eedd4d91db6a45e877c6c8d72ff9a02f96d9
kc8son <kc8son@yahoo.com>
2023-06-16 07:15:58 -0500

    After class - building an API first using FastAPI, then using flask.

--- Day048_BuildAPI/main.py
+++ Day048_BuildAPI/main.py
@@ -1,8 +1,16 @@
+# To Do:
+# Add users
+# delete users
+# Post & put
+# look at limit for arguments
+
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+list_of_usernames = {}
 
 class HelloWorld(Resource):
     def get(self, name, test):
@@ -12,10 +20,17 @@ class HelloWorld(Resource):
 #         return {"passed string": name, "passed integer": test, "greeting": my_string}
 
     
-    def post(self):
-        return {"Hello world": "posted"}
+    def post(self, user_name):
+        # return {"Hello world": "posted"}
+        list_of_usernames.append(user_name)
+        return {
+            "username": list_of_usernames
+        }
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+
+
+# api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/")
 
 if __name__ == "__main__":
     app.run(debug=True)
